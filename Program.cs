46eb384c9c12a0739433385fbc222229297786c1
qigangzhong <qigangzhong@hotmail.com>
2016-11-17 23:00:43 +0800

    my first .net core console app

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name=System.Environment.MachineName;
+            
+            MyClass myClass=new MyClass(){
+                Id=1,
+                Name="zhangsan"
+            };
+            System.Console.WriteLine(myClass.Name);
+        }
+    }
+
+    public class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
